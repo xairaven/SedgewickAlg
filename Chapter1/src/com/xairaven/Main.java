1f417816398c6680af5b7ea106409dab72da990e
xairaven <alexkovalevkpi@gmail.com>
2022-01-07 16:17:53 +0200

    Test commit 2

--- Chapter1/src/com/xairaven/Main.java
+++ Chapter1/src/com/xairaven/Main.java
@@ -3,6 +3,6 @@ package com.xairaven;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World TEST");
     }
 }
