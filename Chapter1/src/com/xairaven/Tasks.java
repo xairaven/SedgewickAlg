f22ed9a2db46ce7e9763be641e6e22b3497f4b25
xairaven <alexkovalevkpi@gmail.com>
2022-01-12 15:32:38 +0200

    added solutions up to task 1.1.7

--- Chapter1/src/com/xairaven/Tasks.java
+++ Chapter1/src/com/xairaven/Tasks.java
@@ -52,7 +52,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-  public void task116() {
+    public void task116() {
         System.out.println("-- Task 1.1.6 --");
         int f = 0;
         int g = 1;
@@ -63,4 +63,32 @@ public class Tasks {
         }
         System.out.print("\n\n");
     }
+
+     public void task117() {
+        System.out.println("-- Task 1.1.7 --");
+        System.out.print("\ta)");
+        double t = 9.0;
+        while (Math.abs(t - 9.0 / t) > .001) {
+            t = (9.0 / t + t) / 2.0;
+        }
+        StdOut.printf("\t%.5f\n", t);
+        System.out.print("\tb)");
+        int sum = 0;
+        for (int i = 1; i < 1000; i++) {
+            for (int j = 0; j < i; j++) {
+                sum++;
+            }
+        }
+        StdOut.printf("\t%d\n", sum);
+        System.out.print("\tc)");/*
+        int sum = 0;
+        for (int i = 1; i < 1000; i *= 2) {
+            for (int j = 0; j < N; j++) {
+                sum++;
+            }
+        }
+        StdOut.printf("\t%d\n", sum);*/
+        StdOut.printf("\tError.");
+     }
+
 }
