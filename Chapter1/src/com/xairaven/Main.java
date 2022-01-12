4b09bab226ec97be68e89308005ec1f4319465ba
xairaven <alexkovalevkpi@gmail.com>
2022-01-12 17:30:37 +0200

    added solutions up to task 1.1.14

--- Chapter1/src/com/xairaven/Main.java
+++ Chapter1/src/com/xairaven/Main.java
@@ -4,15 +4,42 @@ public class Main {
     public static void main(String[] args) {
         Tasks tasks = new Tasks();
         /*
-        tasks.task111();
-        tasks.task112();
-        tasks.task113(args); // Main -> Edit configurations -> Program Arguments -> Your input
-        tasks.task114();
-        tasks.task115(0.5, 1);
-        tasks.task116();
-        tasks.task117();
-        tasks.task118();
-        tasks.task119(52);   //Converting from decimal to binary
-        tasks.task1110();*/
+        tasks.task1_1_1();
+        tasks.task1_1_2();
+        tasks.task1_1_3(args); // Main -> Edit configurations -> Program Arguments -> Your input
+        tasks.task1_1_4();
+        tasks.task1_1_5(0.5, 1);
+        tasks.task1_1_6();
+        tasks.task1_1_7();
+        tasks.task1_1_8();
+        tasks.task1_1_9(52);   //Converting from decimal to binary
+        tasks.task1_1_10();
+        ////////////////
+        boolean[][] arr1_1_10 = {
+                {true, false, true, true},
+                {true, true, true, true},
+                {false, false, true, true},
+                {true, false, false, true}
+        };
+        tasks.task1_1_11(arr1_1_10);
+        /////////////////
+        tasks.task1_1_12();
+        /////////////////
+        int[][] arr1_1_13 = {
+                {1, 2, 3, 4, 5},
+                {5, 7, 9, 8, 6},
+                {1, 5, 6, 7, 7},
+                {2, 3, 2, 3, 9}
+        };
+        int[][] arr_1_1_13 = tasks.task1_1_13(arr1_1_13);
+        for (int i = 0; i < arr_1_1_13.length; i++) {
+            for (int j = 0; j < arr_1_1_13[0].length; j++) {
+                System.out.printf("%d\t", arr_1_1_13[i][j]);
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n\n");
+        /////////////////*/
+        tasks.task1_1_14();
     }
 }
--- Chapter1/src/com/xairaven/Tasks.java
+++ Chapter1/src/com/xairaven/Tasks.java
@@ -2,7 +2,7 @@ package com.xairaven;
 import edu.princeton.cs.introcs.*;
 
 public class Tasks {
-    public void task111() { //11.01.2021
+    public void task1_1_1() { //11.01.2021
         System.out.println("-- Task 1.1.1 --");
         System.out.println((0 + 15) / 2);
         System.out.println(2.0e-6 * 100000000.1);
@@ -10,7 +10,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-    public void task112() {
+    public void task1_1_2() {
         System.out.println("-- Task 1.1.2 --");
         System.out.println((1 + 2.236) / 2);
         System.out.println(1 + 2 + 3 + 4.0);
@@ -19,7 +19,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-    public void task113(String[] args) {
+    public void task1_1_3(String[] args) {
         System.out.println("-- Task 1.1.3 --");
         if (args.length == 3) {
             if(args[0].equals(args[1]) && args[1].equals(args[2])) {
@@ -33,7 +33,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-    public void task114() {
+    public void task1_1_4() {
         System.out.println("-- Task 1.1.4 --");
         System.out.println("if (a>b) then c < 0\n\t\tKey word then is not part of construction IF");
         System.out.println("a > b { c = 0; }\n\t\tMissing IF");
@@ -42,7 +42,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-    public void task115(double x, double y) {
+    public void task1_1_5(double x, double y) {
         System.out.println("-- Task 1.1.5 --");
         if ((x >= 0 && x <=1) && (y >= 0 && y <= 1)) {
             System.out.println("True");
@@ -52,7 +52,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-    public void task116() {
+    public void task1_1_6() {
         System.out.println("-- Task 1.1.6 --");
         int f = 0;
         int g = 1;
@@ -64,7 +64,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-    public void task117() {
+    public void task1_1_7() {
         System.out.println("-- Task 1.1.7 --");
         System.out.print("\ta)");
         double t = 9.0;
@@ -92,7 +92,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-    public void task118() {
+    public void task1_1_8() {
         System.out.println("-- Task 1.1.8 --");
         System.out.print("\ta)\t");
         System.out.print('b');
@@ -103,7 +103,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-    public void task119 (int N) {
+    public void task1_1_9 (int N) {
         System.out.println("-- Task 1.1.8 --");
         String s = "";
         for (int n = N; n > 0; n /= 2) {
@@ -113,7 +113,7 @@ public class Tasks {
         System.out.print("\n\n");
     }
 
-    public void task1110() {
+    public void task1_1_10() {
         System.out.println("-- Task 1.1.10 --");
         /*
         int[] a;
@@ -122,5 +122,50 @@ public class Tasks {
         }*/
         System.out.println("Variable a might not be initialised");
         System.out.print("\n\n");
+    }
+
+    public void task1_1_11(boolean[][] a) {
+        System.out.println("-- Task 1.1.11 --");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                if (a[i][j]) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n\n");
+    }
+
+    public void task1_1_12() {
+        System.out.println("-- Task 1.1.12 --");
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++) {
+            a[i] = 9 - i;
+        }
+        for(int i = 0; i < 10; i++) {
+            a[i] = a[a[i]];
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("\t%d\n", a[i]);
+        }
+        System.out.print("\n\n");
+    }
+
+    public int[][] task1_1_13(int[][] arr) {
+        System.out.println("-- Task 1.1.13 --");
+        int[][] arr_T = new int[arr[0].length][arr.length];
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr_T[i][j] = arr[j][i];
+            }
+        }
+        return arr_T;
+    }
+
+    public void task1_1_14() {
+        System.out.println("-- Task 1.1.14 --");
     }
 }
